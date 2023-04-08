3da9e3eac142f46c8520a3a0f5edae5f2cd50b88
JoaoPeres <jperesbertoncelo@gmail.com>
2023-04-08 20:32:47 +0044

    trabalho 3 adicionado

---
+++ Trabalho 3 - Ant System/Caixeiro viajante Ant-System.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def ant_system_corte(num_formigas, num_iteracoes, alpha, beta, rho, Q, itens, capacidade):
+    num_itens = len(itens)
+    trilhas_feromonio = np.ones(num_itens) / num_itens
+    melhor_solucao = None
+    melhor_valor = float('-inf')
+
+    for it in range(num_iteracoes):
+        solucoes_formigas = []
+
+        # Construção de soluções por formigas
+        for k in range(num_formigas):
+            capacidade_restante = capacidade
+            solucao_formiga = np.zeros(num_itens)
+            indices_disponiveis = np.arange(num_itens)
+
+            while len(indices_disponiveis) > 0:
+                probs = trilhas_feromonio[indices_disponiveis] * alpha * (itens[indices_disponiveis, 0] * beta) / (1 + itens[indices_disponiveis, 1])
+                probs /= probs.sum()
+
+                i = np.random.choice(indices_disponiveis, p=probs)
+                capacidade_restante -= itens[i, 1]
+
+                if capacidade_restante < 0:
+                    break
+
+                solucao_formiga[i] = 1
+                indices_disponiveis = np.delete(indices_disponiveis, np.argwhere(indices_disponiveis == i))
+
+            valor_solucao_formiga = np.dot(solucao_formiga, itens[:, 0])
+            if valor_solucao_formiga > melhor_valor:
+                melhor_solucao = solucao_formiga
+                melhor_valor = valor_solucao_formiga
+
+            solucoes_formigas.append((solucao_formiga, valor_solucao_formiga))
+
+        # Atualização das trilhas de feromônio
+        trilhas_feromonio *= (1 - rho)
+
+        for solucao_formiga, valor_solucao_formiga in solucoes_formigas:
+            for i, item in enumerate(solucao_formiga):
+                if item == 1:
+                    trilhas_feromonio[i] += Q / valor_solucao_formiga
+
+    # execute a função para obter a melhor solução e o melhor valor
+    melhor_solucao, melhor_valor = ant_system_corte(num_formigas, num_iteracoes, alpha, beta, rho, Q, itens, capacidade)
+
+    # plota um gráfico de barras que mostra a quantidade de cada item que foi selecionado na solução encontrada 
+    plt.bar(range(len(melhor_solucao)), melhor_solucao)
+    plt.xlabel('Item')
+    plt.ylabel('Quantidade')
+    plt.title('Itens selecionados na solução')
+    plt.show()
+
+    print(f"Melhor solução: {melhor_solucao}")
+    print(f"Melhor valor: {melhor_valor}\n")
+
+    return melhor_solucao, melhor_valor
+
+if __name__ == '__main__':
+    num_formigas = 10
+    num_iteracoes = 100
+    alpha = 1
+    beta = 5
+    rho = 0.5
+    Q = 1
+    itens = np.array([[10, 3], [15, 4], [20, 2], [7, 1], [8, 2], [5, 3]])
+    capacidade = 10
+    ant_system_corte(num_formigas, num_iteracoes, alpha, beta, rho, Q, itens, capacidade)
+    
